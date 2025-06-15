82e79ee0029a688deeeadc0ab7864eb07bf3b0a8
OCOS UK <support@ocos.io>
2025-06-15 20:16:45 +0400

    constants.ts

---
+++ src/components/WalletConnect/constants.ts
@@ -0,0 +1,7 @@
+export const SUPPORTED_WALLETS = ["MetaMask", "WalletConnect"] as const;
+
+export const SUPPORTED_CHAINS = [
+  { name: "Binance Smart Chain", id: 56 },
+  { name: "Ethereum", id: 1 },
+  // İstəyə görə əlavə edilə bilər
+];
