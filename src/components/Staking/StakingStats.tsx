4308eebcaf6fbb58f85e4c264ce08c4485f35e84
OCOS UK <support@ocos.io>
2025-06-16 00:22:37 +0400

    StakingStats.tsx

---
+++ src/components/Staking/StakingStats.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StakingStats: React.FC = () => (
+  <div className="staking-stats">
+    <div>Staking APR: <strong>28.5%</strong></div>
+    <div>Toplam Staking: <strong>1,250,000 OCOS</strong></div>
+    <div>Hovuz TVL: <strong>2,430,000 USDT</strong></div>
+  </div>
+);
+
+export default StakingStats;
