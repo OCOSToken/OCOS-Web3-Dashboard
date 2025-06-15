7b27945073b7b6aee85d8559d6a94a20b6d06933
OCOS UK <support@ocos.io>
2025-06-16 00:24:23 +0400

    index.ts

---
+++ src/components/Staking/index.ts
@@ -0,0 +1,7 @@
+export { default as StakingDashboard } from "./StakingDashboard";
+export { default as StakeForm } from "./StakeForm";
+export { default as StakingStats } from "./StakingStats";
+export { default as StakingRewards } from "./StakingRewards";
+export { default as StakeHistory } from "./StakeHistory";
+export { default as StakingInfoModal } from "./StakingInfoModal";
+export * from "./types";
