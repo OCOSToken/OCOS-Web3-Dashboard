0fac2b70f34031dfd350e315927642a9d05b0fac
OCOS UK <support@ocos.io>
2025-06-16 00:27:24 +0400

    AirdropStatus.tsx

---
+++ src/components/Airdrop/AirdropStatus.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AirdropStatus: React.FC = () => (
+  <div>
+    <span className="status success">Uygunsunuz!</span>
+    {/* Yoxsa <span className="status failed">Uygun değilsiniz</span> */}
+  </div>
+);
+
+export default AirdropStatus;
