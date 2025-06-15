818085a8c60213bbeff8809c6afa873455202ca6
OCOS UK <support@ocos.io>
2025-06-16 00:27:57 +0400

    AirdropHistory.tsx

---
+++ src/components/Airdrop/AirdropHistory.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AirdropHistory: React.FC = () => (
+  <div>
+    <h4>Airdrop Geçmişiniz</h4>
+    <ul>
+      <li>12.06.2025 – 1,000 OCOS – Başarılı</li>
+      <li>08.06.2025 – 800 OCOS – Başarılı</li>
+    </ul>
+  </div>
+);
+
+export default AirdropHistory;
