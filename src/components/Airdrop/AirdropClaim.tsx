af81fe31975f65e1c909d7283faa361c8567923d
OCOS UK <support@ocos.io>
2025-06-16 00:27:06 +0400

    AirdropClaim.tsx

---
+++ src/components/Airdrop/AirdropClaim.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AirdropClaim: React.FC = () => (
+  <div>
+    <button className="claim-btn">Airdrop’u Talep Et</button>
+  </div>
+);
+
+export default AirdropClaim;
